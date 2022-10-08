9e3293b756cbc68b43b46c3f0229f73f50aceeed
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-10-09 02:11:37 +0700

    update exercise-pointer

--- exercise/exercise2/golang-slurred-talk-v1/main.go
+++ exercise/exercise2/golang-slurred-talk-v1/main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func SlurredTalk(words *string) {
+	arrGenerate := strings.Split(*words, "")
+	vokal := []string{"s", "r", "z", "S", "R", "Z"}
+	output := ""
+	for i := 0; i < len(arrGenerate); i++ {
+		a := arrGenerate[i]
+		for j := 0; j < len(vokal); j++ {
+			if a == vokal[j] {
+				if strings.ToUpper(a) == a {
+					a = "L"
+				} else {
+					a = "l"
+				}
 
+				break
+			}
+		}
+
+		output += a
+	}
+	fmt.Println(output)
+	*words = output
 }
 
 func main() {
 	// bisa dicoba untuk pengujian test case
-	var words string = "Steven is me"
+	var words string = "Streven is meZ"
 	SlurredTalk(&words)
 	fmt.Println(words)
 }
--- exercise/exercise2/phone-number-checker-v1/main.go
+++ exercise/exercise2/phone-number-checker-v1/main.go
@@ -2,15 +2,109 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func PhoneNumberChecker(number string, result *string) {
-	// TODO: answer here
+	arrNumber := strings.Split(number, "")
+	a := arrNumber[0] + arrNumber[1] + arrNumber[2]
+	b := arrNumber[0] + arrNumber[1]
+	if (a == "628") && (len(arrNumber) >= 11) {
+		c := arrNumber[3] + arrNumber[4]
+		var cRes string
+		findProvider(c, &cRes)
+		*result = cRes
+	} else if (b == "08") && (len(arrNumber) >= 10) {
+		c := arrNumber[2] + arrNumber[3]
+		var cRes string
+		findProvider(c, &cRes)
+		*result = cRes
+	} else {
+		*result = "invalid"
+	}
+}
+
+func findProvider(try string, rest *string) {
+	arrTelkom := []string{"11", "12", "13", "14", "15"}
+	arrIndosat := []string{"16", "17", "18", "19"}
+	arrXl := []string{"21", "22", "23"}
+	arrTri := []string{"27", "28", "29"}
+	arrAs := []string{"52", "53"}
+	arrSf := []string{"81", "82", "83", "84", "85", "86", "87", "88"}
+	a := false
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println("hai")
+	// 	if try == arrTelkom[i] {
+	// 		*rest = "Telkomsel"
+	// 		a = true
+
+	// 	} else if try == arrIndosat[i] {
+	// 		*rest = "Indosat"
+	// 		a = true
+
+	// 	} else if try == arrXl[i] {
+	// 		*rest = "Xl"
+	// 		a = true
+
+	// 	} else if try == arrTri[i] {
+	// 		*rest = "Tri"
+	// 		a = true
+
+	// 	} else if try == arrAs[i] {
+	// 		*rest = "As"
+	// 		a = true
+
+	// 	} else if try == arrSf[i] {
+	// 		*rest = "Smartfren"
+	// 		a = true
+	// 	} else {
+	// 		a = false
+	// 	}
+	// }
+	for _, v := range arrTelkom {
+		if try == v {
+			*rest = "Telkomsel"
+			a = true
+		}
+	}
+	for _, v := range arrIndosat {
+		if try == v {
+			*rest = "Indosat"
+			a = true
+		}
+	}
+	for _, v := range arrAs {
+		if try == v {
+			*rest = "AS"
+			a = true
+		}
+	}
+	for _, v := range arrXl {
+		if try == v {
+			*rest = "XL"
+			a = true
+		}
+	}
+	for _, v := range arrTri {
+		if try == v {
+			*rest = "Tri"
+			a = true
+		}
+	}
+	for _, v := range arrSf {
+		if try == v {
+			*rest = "Smartfren"
+			a = true
+		}
+	}
+	if a == false {
+		*rest = "invalid"
+	}
 }
 
 func main() {
 	// bisa digunakan untuk pengujian test case
-	var number = ""
+	var number = "081434561234"
 	var result string
 
 	PhoneNumberChecker(number, &result)
