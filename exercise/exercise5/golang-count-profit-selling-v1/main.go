a45cb489ea230420f9f6f0e6be441669734bf9f9
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-10-11 16:56:09 +0700

    done schedule task

--- exercise/exercise5/golang-count-profit-selling-v1/main.go
+++ exercise/exercise5/golang-count-profit-selling-v1/main.go
@@ -1,5 +1,56 @@
 package main
 
+import "fmt"
+
 func CountProfit(data [][][2]int) []int {
-	return nil // TODO: replace this
+	a := []int{}
+	b := []int{}
+	c := []int{}
+	count := 0
+	sum := 0
+	if len(data) == 0 {
+
+	} else {
+		for i := 0; i < len(data); i++ {
+			fmt.Println("1", i)
+			hum := 0
+			for j := 0; j < len(data[i]); j++ {
+				fmt.Println("2", j)
+				sum := 0
+				for k := 0; k < len(data[i][j]); k++ {
+					fmt.Println("3", k)
+					if sum == 0 {
+						sum += data[i][j][k]
+					} else {
+						hehe := sum - data[i][j][k]
+						count = hehe
+						fmt.Println("count: ", count)
+						a = append(a, count)
+					}
+				}
+				hum += count
+				fmt.Println("hum: ", hum)
+			}
+			b = append(b, hum)
+			sum += hum
+			fmt.Println("sum: ", b)
+		}
+		c = append(c, sum)
+		fmt.Println("sum: ", c)
+	}
+
+	return a // TODO: replace this
+}
+
+func main() {
+	a := [][][2]int{
+		{
+			{1000, 500},
+			{500, 150}}, {
+			{600, 100},
+			{800, 750},
+		},
+	}
+
+	fmt.Println(CountProfit(a))
 }
--- exercise/exercise5/schedulable-days-2-v1/main.go
+++ exercise/exercise5/schedulable-days-2-v1/main.go
@@ -1,9 +1,45 @@
 package main
 
+import (
+	"fmt"
+)
+
 func SchedulableDays(villager [][]int) []int {
-	return nil // TODO: replace this
+	res := []int{}
+	a := []int{}
+	for i := 0; i < len(villager); i++ {
+		for j := 0; j < len(villager[i]); j++ {
+			a = append(a, villager[i][j])
+		}
+	}
+	fmt.Println(a)
+	if len(villager) <= 1 {
+		res = a
+	} else {
+		keys := make(map[int]bool)
+		for _, entry := range a {
+			if _, value := keys[entry]; !value {
+				if !value {
+					keys[entry] = true
+				}
+			} else {
+				res = append(res, entry)
+			}
+		}
+	}
+
+	return res // TODO: replace this
 }
 
 func main() {
-a:= 
+	a := [][]int{
+		// {1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 19, 20},
+		// {3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 19, 20},
+		// {1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 19, 20},
+		// {15, 16, 17, 18, 19, 20},
+		// {1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 19, 20},
+		// {1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 19, 20},
+		{1, 2, 3},
+	}
+	fmt.Println(SchedulableDays(a))
 }
