6d435d51c6efcfb5cafdaf78de58c2e043e1e18e
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-10-13 20:00:18 +0700

    add assignement3

--- exercise/exercise8/package-import-cp-1-v3/main.go
+++ exercise/exercise8/package-import-cp-1-v3/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-
 func AdvanceCalculator(calculate string) float32 {
+	a := strings.Split(calculate, " ")
+	fmt.Println(a)
+	cal := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] == "*" {
+			if cal == 0 {
+				bil1, _ := strconv.Atoi(a[i-1])
+				bil2, _ := strconv.Atoi(a[i+1])
+				cal = bil1 * bil2
+				
+			} else {
+
+			}
+		} else if a[i] == "/" {
+
+		}
+	}
+	fmt.Println(cal)
 	return 0 // TODO: replace this
 }
 
