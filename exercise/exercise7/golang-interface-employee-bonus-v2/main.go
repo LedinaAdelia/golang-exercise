d2bde44ba963e9a856cf2947903ff5984c9b6caa
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-10-14 11:34:23 +0700

    done exercise 7

--- exercise/exercise7/golang-interface-employee-bonus-v2/main.go
+++ exercise/exercise7/golang-interface-employee-bonus-v2/main.go
@@ -1,56 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Employee interface {
 	GetBonus() float64
 }
 
 type Junior struct {
-	Name         string
-	BaseSalary   int
-	WorkingMonth int
+	Name                     string
+	BaseSalary, WorkingMonth int
 }
 
 type Senior struct {
-	General         Junior
-	PerformanceRate float64
+	Name                     string
+	BaseSalary, WorkingMonth int
+	PerformanceRate          float64
 }
 
 type Manager struct {
-	General          Senior
-	BonusManagerRate float64
+	Name                              string
+	BaseSalary, WorkingMonth          int
+	PerformanceRate, BonusManagerRate float64
+}
+
+func (j *Junior) GetBonus() float64 {
+	sal := float64(j.BaseSalary)
+	work := float64(j.WorkingMonth)
+	works := work / 12
+	count := 1 * sal * works
+	fmt.Println("hehe: ", count)
+	return count
+}
+
+func (s *Senior) GetBonus() float64 {
+	sal := float64(s.BaseSalary)
+	work := float64(s.WorkingMonth)
+	works := work / 12
+	perfR := s.PerformanceRate * sal
+	count := (2 * sal * works) + perfR
+	return count
+}
+
+func (m *Manager) GetBonus() float64 {
+	sal := float64(m.BaseSalary)
+	work := float64(m.WorkingMonth)
+	works := work / 12
+	perfR := m.PerformanceRate * sal
+	mngB := m.BonusManagerRate * sal
+	count := (2 * sal * works) + perfR + mngB
+	return count
 }
 
 func EmployeeBonus(employee Employee) float64 {
-	return 0.0 // TODO: replace this
+	return employee.GetBonus() // TODO: replace this
 }
 
 func TotalEmployeeBonus(employees []Employee) float64 {
-	fmt.Println(employees)
-	return 0.0 // TODO: replace this
+	res := 0.0
+	for _, v := range employees {
+		total := EmployeeBonus(v)
+		res += total
+	}
+	return res // TODO: replace this
 }
 
 func main() {
-	Bonus := []Employee{
-		Junior{
-			Name:         "Della",
-			BaseSalary:   1000000,
+	employee := []Employee{
+		&Junior{
+			Name:         "Senior Engineer",
+			BaseSalary:   100000,
 			WorkingMonth: 12,
 		},
-		Senior{
-			General.Name:         "Ledina",
-			General.BaseSalary:   100000,
-			General.WorkingMonth: 12,
-			PerformanceRate:      0.5,
+		&Junior{
+			Name:         "Senior Engineer 2",
+			BaseSalary:   120000,
+			WorkingMonth: 4,
 		},
-		Manager{
-			General.Name:            "Adelia",
-			General.BaseSalary:      100000,
-			General.WorkingMonth:    12,
-			General.PerformanceRate: 0.5,
-			BonusManagerRate:        0.5,
-		}
 	}
-	fmt.Println(Bonus)
+
+	res2 := TotalEmployeeBonus(employee)
+	fmt.Println(res2)
 }
