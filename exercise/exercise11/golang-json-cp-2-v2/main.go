022b266697e446ef8c27213575c325d8a2be3178
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-10-18 15:48:51 +0700

    done exercise 11 - 2

--- exercise/exercise11/golang-json-cp-2-v2/main.go
+++ exercise/exercise11/golang-json-cp-2-v2/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
 )
 
 type LoanData struct {
@@ -23,15 +27,69 @@ type Employee struct {
 
 // json structure
 type LoanRecord struct {
+	MonthDate    string     `json:"month_date"`
+	StartBalance float64    `json:"start_balance"`
+	EndBalance   float64    `json:"end_balance"`
+	Borrowers    []Borrower `json:"borrowers"`
 }
 
-
+type Borrower struct {
+	Id         string  `json:"id"`
+	Total_loan float64 `json:"total_loan"`
+}
 
 func LoanReport(data LoanData) LoanRecord {
-	return LoanRecord{} // TODO: replace this
+	recLoan := LoanRecord{}
+	var loan float64 = 50000
+	try := map[string]float64{}
+	sBal := data.StartBalance
+	date := ""
+	for _, a := range data.Data {
+		date = a.Date
+		for _, b := range a.EmployeeIDs {
+			if sBal <= 0 {
+				break
+			} else if sBal > 1 {
+				try[b] += loan
+				sBal -= int(loan)
+			}
+		}
+	}
+
+	keys := make([]string, 0, len(try))
+	for k := range try {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	fixDate := ""
+	if date == "" {
+
+	} else {
+		formatDate := strings.Split(date, "-")
+		fixDate = formatDate[1] + " " + formatDate[2]
+	}
+	recLoan.MonthDate = fixDate
+	recLoan.StartBalance = float64(data.StartBalance)
+	recLoan.EndBalance = float64(sBal)
+
+	for _, v := range keys {
+		recLoan.Borrowers = append(recLoan.Borrowers, Borrower{v, try[v]})
+	}
+	return recLoan
+
 }
 
 func RecordJSON(record LoanRecord, path string) error {
+	var jsonData, err = json.Marshal(record)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(jsonData)
+	err = ioutil.WriteFile(path, jsonData, 0644)
+	if err != nil {
+		panic(err)
+	}
 	return nil // TODO: replace this
 }
 
@@ -56,6 +114,4 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	fmt.Println(records)
 }
