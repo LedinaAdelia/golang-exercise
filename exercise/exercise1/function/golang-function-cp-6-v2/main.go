3a7c6ed4f0f8a1fd5107968b0775dbf4d21b452f
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-12-30 18:58:57 +0700

    update exercise function(6)

--- exercise/exercise1/function/golang-function-cp-6-v2/main.go
+++ exercise/exercise1/function/golang-function-cp-6-v2/main.go
@@ -3,53 +3,39 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 func ChangeToCurrency(change int) string {
 	arrChange := strconv.Itoa(change)
-	splitChange := strings.Split(arrChange, "")
 	cResultReverse := ""
 	cResult := ""
 	count := 0
-	for i := 0; i < len(splitChange); i++ {
-		if count == 2 && i != (len(splitChange)-1) {
+	for i := 0; i < len(arrChange); i++ {
+		if count == 2 && i != (len(arrChange)-1) {
+			cResultReverse += string(arrChange[len(arrChange)-i-1]) + "."
 			count = 0
-			cResultReverse += splitChange[len(splitChange)-i-1] + "."
-
 		} else {
+			cResultReverse += string(arrChange[len(arrChange)-i-1])
 			count++
-			cResultReverse += splitChange[len(splitChange)-i-1]
 		}
 	}
-	result := strings.Split(cResultReverse, "")
-	for j := 0; j < len(result); j++ {
-		cResult += result[len(result)-j-1]
+	for j := 0; j < len(cResultReverse); j++ {
+		cResult += string(cResultReverse[len(cResultReverse)-j-1])
 	}
-
 	return fmt.Sprintf("Rp. %s", cResult)
 }
 
 func MoneyChange(money int, listPrice ...int) string {
-	arrData := []int(listPrice)
 	totalPrice := 0
-	result := ""
-	for i := 0; i < len(arrData); i++ {
-		totalPrice += arrData[i]
+	for _, v := range listPrice {
+		totalPrice += v
 	}
-	cData := money - totalPrice
-	if cData == 0 {
-		result = ChangeToCurrency(cData)
-	} else if cData < 0 {
-		result = "Uang tidak cukup"
-	} else {
-		result = ChangeToCurrency(cData)
+	if money < totalPrice {
+		return "Uang tidak cukup"
 	}
-	return result // TODO: replace this
+	return ChangeToCurrency(money - totalPrice)
 }
 
-// gunakan untuk melakukan debug
 func main() {
 	fmt.Println(MoneyChange(100000, 50000, 10000, 10000, 5000, 5000))
-	fmt.Println(MoneyChange(10000, 5000, 5000))
 }
