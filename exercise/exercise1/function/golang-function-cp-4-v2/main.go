5681d14d18675f258eae8c544d7d25301e812e0d
Ledina Adelia Putri Siregar <20082010109@student.upnjatim.ac.id>
2022-12-30 17:40:26 +0700

    update exercise function-4

--- exercise/exercise1/function/golang-function-cp-4-v2/main.go
+++ exercise/exercise1/function/golang-function-cp-4-v2/main.go
@@ -6,22 +6,18 @@ import (
 )
 
 func FindSimilarData(input string, data ...string) string {
-	arrData := []string(data)
-	result := ""
-	for i := 0; i < len(arrData); i++ {
-		if strings.Contains(arrData[i], input) {
-			result += arrData[i] + ","
+	res := ""
+	for i := 0; i < len(data); i++ {
+		if strings.Contains(data[i], input) {
+			res += data[i] + ","
 		}
 	}
-	s1 := result
-	if last := len(s1) - 1; last >= 0 && s1[last] == ',' {
-		s1 = s1[:last]
+	if last := len(res) - 1; last >= 0 && res[last] == ',' {
+		res = res[:last]
 	}
-	return s1 // TODO: replace this
+	return res
 }
 
-// gunakan untuk melakukan debug
 func main() {
 	fmt.Println(FindSimilarData("iphone", "laptop", "iphone 13", "iphone 12", "iphone 12 pro"))
-	fmt.Println(FindSimilarData("mobil", "mobil APV", "mobil Avanza", "motor matic", "motor gede"))
 }
